f034f542250262c62919ddd02cfa01740d859ee3
nadiamonse <nadia.monserrat89@gmail.com>
2025-07-08 19:21:17 -0300

    creacion

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let num1= 1
+let num2= 2
+let num3= 3
+
+
+let numeros= [1, 2, 3, ]
+console.log (numeros)
+ alert()
+
+let alunmo1 = "perez"
+let alumno2 = "monserrat"
+
+let alumnos = new Array("perez", "monserrat", )
+console.log(alumnos);
