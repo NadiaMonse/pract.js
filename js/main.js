21e6905887f6733049af30edee7fa95704a8f716
nadiamonse <nadia.monserrat89@gmail.com>
2025-07-17 01:59:40 -0300

    se agrego iteracion

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 const nombres = ["ana", "sole", "nadia",];
+console.log(nombres);
 
 function saludar(nombre){
-    alert("hola,"+ nombre+"!");
+    alert("hola," + nombre +"!");
 }
+for(let i = 0;i<nombres.length;i++){
+    console.log(`Hola ${nombres[i]}`);
+}
+
 let nombreIngresado = prompt( "ingresa tu nombre");
 
 if(nombres.includes(nombreIngresado)){
