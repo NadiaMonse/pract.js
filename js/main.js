3a1da12c8a20a0bef4974a53fb4570afad455d5a
nadiamonse <nadia.monserrat89@gmail.com>
2025-07-29 19:41:19 -0300

    entrega incompleta

--- js/main.js
+++ js/main.js
@@ -1,17 +1,34 @@
-const nombres = ["ana", "sole", "nadia",];
-console.log(nombres);
+alert()
 
-function saludar(nombre){
-    alert("hola," + nombre +"!");
-}
-for(let i = 0;i<nombres.length;i++){
-    console.log(`Hola ${nombres[i]}`);
+const productos=[
+    {id: 1,nombre:"camiseta",precio:2500},
+    {id: 2,nombre:"pantalon",precio:4500},
+    {id: 3,nombre:"zapatillas",precio:8000}, 
+];
+
+let carrito= JSON.parse(localStorage.getItem("carrito")
+
+
+function mostrarProductos(){
+    productos.forEach
+
+function agregarAlCarrito(id){
+    const producto = productos.find(p=>p.id===id);
+    carrito.push(producto);
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+    mostrarCarrito();
 }
 
-let nombreIngresado = prompt( "ingresa tu nombre");
-
-if(nombres.includes(nombreIngresado)){
-    saludar(nombreIngresado);
-}else{
-    alert("nombre no reconocido");
+function monstrarCarrito(){
+    listaCarrito.innerHTML="";
+    carrito.forEach(item=>{
+        const li = document.createElement("li");
+        li.textContent = `${item.nombre} - $${item.precio}`;
+        listaCarrito.appendChild(li);
+    });
 }
+
+
+
+mostrarProductos();
+mostrarCarrito();
